9c398bd25f276dda77d3fbbc77efcf269f633372
David Paris <parisd1992@gmail.com>
2019-03-04 07:50:46 +0000

    Test GetTank method via print statement

--- BattleTanks/Source/BattleTanks/Private/TankPlayerController.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankPlayerController.cpp
@@ -2,6 +2,20 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+    auto controlledTank = GetControlledTank();
+    if (controlledTank)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("PlayerController controlled tank: %s"), *(controlledTank->GetName()));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("PlayerController no controlled tank"));
+    }
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
     return Cast<ATank>(GetPawn());
